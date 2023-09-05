a4c415e8bbbba429e4f08dae21a032c9f81aa124
Snorre <snle001@student.kristiania.no>
2023-09-05 11:11:18 +0200

    asdasd

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
-        System.out.println();
-        System.out.println();
     }
 }
